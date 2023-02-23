c0fa32d92a9a42b95ad32c994093e5504aa66ebd
Samadhan Patil <109036065+PatilSama@users.noreply.github.com>
2023-02-23 16:51:45 +0530

    all

--- app/src/main/java/com/example/citybus/ConstantClass/Model1.java
+++
@@ -1,7 +0,0 @@
-package com.example.citybus.ConstantClass;
-
-public class Model1 {
-    public static String SelectType = "";
-    public static String stop_From = "";
-    public static String stop_To = "";
-}
